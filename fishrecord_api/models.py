03b4f49c3dbe50ae77ca2aff1785302c04a814c8
Ashish Kaul <ashishkaul@Ashishs-MacBook-Air.local>
2022-03-25 19:15:05 +0530

    Created models

--- fishrecord_api/models.py
+++ fishrecord_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Record(models.Model):
+    name = models.CharField(max_length=255)
+    species = models.CharField(max_length=255)
+    weight = models.FloatField()
+    length = models.FloatField()
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/', null=True, default=None)
+
+    def __str__(self):
+        return self.name
